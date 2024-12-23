4ebd97085d4cbb7b82094ae672b72209f9db9a3b
sakib <sbsakibsarkar8@gmail.com>
2024-12-24 04:45:36 +0600

    incident data add

---
+++ src/data/incident/Incidentdata.js
@@ -0,0 +1,93 @@
+const Incidentdata =[
+    {
+        id: 1,
+        icon: '/img/icon/icon2.png',
+        title: 'Avalanche',
+    },
+    {
+        id: 2,
+        icon: '/img/icon/icon13.png',
+        title: 'Biological',
+    },
+    {
+        id: 3,
+        icon: '/img/icon/icon17.png',
+        title: 'Blizzard',
+    },
+    {
+        id: 4,
+        icon: '/img/icon/icon8.png',
+        title: 'Cold/Freezing',
+    },
+    {
+        id: 5,
+        icon: '/img/icon/icon4.png',
+        title: 'Drought',
+    },
+    {
+        id: 6,
+        icon: '/img/icon/icon6.png',
+        title: 'Earthquake',
+    },
+    {
+        id: 7,
+        icon: '/img/icon/icon3.png',
+        title: 'Flooding',
+    },
+    {
+        id: 8,
+        icon: '/img/icon/icon1.png',
+        title: 'Heat Wave',
+    },
+    {
+        id: 9,
+        icon: '/img/icon/icon12.png',
+        title: 'Hail',
+    },
+    {
+        id: 10,
+        icon: '/img/icon/icon14.png',
+        title: 'Lightning',
+    },
+    {
+        id: 11,
+        icon: '/img/icon/icon7.png',
+        title: 'Man Made',
+    },
+    {
+        id: 12,
+        icon: '/img/icon/icon9.png',
+        title: 'Mudslide',
+    },
+    {
+        id: 13,
+        icon: '/img/icon/icon16.png',
+        title: 'Severe Storm',
+    },
+    {
+        id: 14,
+        icon: '/img/icon/icon16.png',
+        title: 'Strong Wind',
+    },
+    {
+        id: 15,
+        icon: '/img/icon/icon11.png',
+        title: 'Tornado',
+    },
+    {
+        id: 16,
+        icon: '/img/icon/icon5.png',
+        title: 'Tsunami',
+    },
+    {
+        id: 17,
+        icon: '/img/icon/icon15.png',
+        title: 'Volcanic Eruption',
+    },
+    {
+        id: 18,
+        icon: '/img/icon/icon10.png',
+        title: 'Wildfire',
+    },
+]
+export default Incidentdata;
